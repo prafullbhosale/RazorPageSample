ba11ceddd9c7a2579697af330d55fa269b79ae2f
Prafull Bhosale <prbhosal@microsoft.com>
2017-07-04 10:44:33 -0700

    Variations of Empty Razor page

---
+++ RazorPageSample/Pages/Empty/EmptyWithScripts.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageSample.Pages.Empty
+{
+    public class EmptyWithScriptsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
